3f503b552ea5465aee727075e7f719327250cf35
ifeanyi-valentine <urielifeanyi@gmail.com>
2024-11-09 03:38:55 +0100

    Refactored codes: removed redundant codes and added a modularized benchmarking function

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,24 @@ struct BenchmarkResult {
     memory_usage: f64,
 }
 
+fn run_benchmark<F: Fn()>(name: &str, bench_fn: F) -> BenchmarkResult {
+    // Run the function and time it
+    let start = Instant::now();
+    bench_fn();
+    let elapsed = start.elapsed();
+
+    // Memory usage calculation
+    epoch::advance().unwrap();
+    let allocated = stats::allocated::read().unwrap();
+
+    BenchmarkResult {
+        name: name.to_string(),
+        elapsed: elapsed.as_secs_f64(),
+        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
+        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
+    }
+}
+
 fn bench_env_logger() -> BenchmarkResult {
     std::env::set_var("RUST_LOG", "info");
     env_logger::builder()
@@ -32,44 +50,22 @@ fn bench_env_logger() -> BenchmarkResult {
         .ok();
     log::set_max_level(LevelFilter::Info);
 
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        info!(target: "benchmark", "{} {}", MESSAGE,"env_logger");
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "env_logger".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
+    run_benchmark("env_logger", || {
+        for _ in 0..ITERATIONS {
+            info!(target: "benchmark", "{} {}", MESSAGE, "env_logger");
+        }
+    })
 }
 
 fn bench_slog() -> BenchmarkResult {
     let plain = slog_term::PlainSyncDecorator::new(std::io::stdout());
     let root = Logger::root(slog_term::FullFormat::new(plain).build().fuse(), o!());
 
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        slog::info!(root, "{} {}", MESSAGE, "slog");
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "slog".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
+    run_benchmark("slog", || {
+        for _ in 0..ITERATIONS {
+            slog::info!(root, "{} {}", MESSAGE, "slog");
+        }
+    })
 }
 
 fn bench_slog_async() -> BenchmarkResult {
@@ -77,83 +73,66 @@ fn bench_slog_async() -> BenchmarkResult {
     let drain = slog_async::Async::new(slog_term::FullFormat::new(plain).build().fuse())
         //.chan_size(1024 * 9)
         .chan_size(8_991) //8_991 is for 10_000 messages, multiply by ten if the number of messages is multiplied by 10
+        .overflow_strategy(slog_async::OverflowStrategy::Block)
         .build()
         .fuse();
     let root = Logger::root(drain, o!());
 
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        slog::info!(root, "{} {}", MESSAGE, "slog_async");
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "slog_async".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
-}
-
-fn bench_slog_async_block() -> BenchmarkResult {
-    let plain = slog_term::PlainSyncDecorator::new(std::io::stdout());
-
-    // Adjusting channel size and blocking strategy to avoid dropped messages
-    let drain = slog_async::Async::new(slog_term::FullFormat::new(plain).build().fuse())
-        //.chan_size(1024) // Increase channel size
-        .overflow_strategy(slog_async::OverflowStrategy::Block) // Block instead of dropping messages
-        .build()
-        .fuse();
-
-    let root = Logger::root(drain, o!());
-
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        slog::info!(root, "{} {}", MESSAGE, "slog_async_block");
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "slog_async_block".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
+    run_benchmark("slog_async", || {
+        for _ in 0..ITERATIONS {
+            slog::info!(root, "{} {}", MESSAGE, "slog_async");
+        }
+    })
 }
 
 fn bench_tracing() -> BenchmarkResult {
     let subscriber = tracing_subscriber::fmt().with_test_writer().finish();
     tracing::subscriber::set_global_default(subscriber).expect("setting tracing default failed");
 
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        event!(Level::INFO, "{} {}", MESSAGE, "tracing");
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "tracing".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
+    run_benchmark("tracing", || {
+        for _ in 0..ITERATIONS {
+            event!(Level::INFO, "{} {}", MESSAGE, "tracing");
+        }
+    })
 }
 
 fn bench_winston() -> BenchmarkResult {
     let logger = winston::Logger::builder()
         .add_transport(winston::transports::Console::new(None))
+        .channel_capacity(8_991)
+        .backpressure_strategy(winston::BackpressureStrategy::DropCurrent)
+        .format(winston::format::combine(vec![
+            winston::format::timestamp().with_option("format", "%b %d %H:%M:%S%.3f"),
+            winston::format::printf(|info: &winston::format::LogInfo| {
+                let timestamp = info
+                    .get_meta("timestamp")
+                    .map(|value| value.as_str().unwrap_or("")) // Ensure it's a &str
+                    .unwrap_or(""); // If no timestamp, default to an empty string
+
+                format!(
+                    "{} - {}: {}",
+                    //info.get_meta("timestamp").unwrap(),
+                    timestamp,
+                    info.level.to_uppercase(),
+                    info.message,
+                )
+            }),
+            //winston::format::json(),
+        ]))
+        .build();
+
+    run_benchmark("winston", || {
+        for i in 0..ITERATIONS {
+            logger.info(&format!("{}{} {}", MESSAGE, i, "winston"));
+        }
+    })
+}
+
+fn bench_winston_global() -> BenchmarkResult {
+    let options = winston::LoggerOptions::new()
+        .add_transport(winston::transports::Console::new(None))
+        .channel_capacity(8_991)
+        .backpressure_strategy(winston::BackpressureStrategy::DropCurrent)
         .format(winston::format::combine(vec![
             winston::format::timestamp(),
             winston::format::printf(|info: &winston::format::LogInfo| {
@@ -165,33 +144,6 @@ fn bench_winston() -> BenchmarkResult {
                 )
             }),
             //winston::format::json(),
-        ]))
-        .build();
-
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        logger.info(&format!("{} {}", MESSAGE, "winston"));
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "winston".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
-}
-
-fn bench_winston_global() -> BenchmarkResult {
-    let options = winston::LoggerOptions::new()
-        .add_transport(winston::transports::Console::new(None))
-        .format(winston::format::combine(vec![
-            winston::format::timestamp(),
-            winston::format::json(),
         ]));
 
     winston::configure(Some(options));
@@ -200,93 +152,13 @@ fn bench_winston_global() -> BenchmarkResult {
     logger.close();*/
     //let logger = winston::Logger::default();
 
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        winston::log_info!("{} {}", MESSAGE, "winston_global");
-        //logger.info(&format!("{} {}", MESSAGE, "winston_global"));
-    }
-    winston::Logger::shutdown();
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "winston_global".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
-}
-
-fn bench_winston_v2() -> BenchmarkResult {
-    winston::logger_v2::transport::initialize_runtime();
-    let console_transport = winston::logger_v2::transport::create_console_transport();
-    let logger = winston::logger_v2::Logger::new(console_transport);
-
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        logger.log(&format!("{} {}", MESSAGE, "winston_v2"));
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "winston_v2".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
-}
-
-fn bench_winston_v3() -> BenchmarkResult {
-    let console_transport =
-        winston::logger_v3::transports::transport::Transport::new(std::io::stdout(), None);
-
-    /*let logger = winston::logger_v3::Logger::builder()
-            .level("info")
-            .format(winston::format::combine(vec![
-                winston::format::timestamp(),
-                winston::format::printf(|info: &winston::format::LogInfo| {
-                    format!(
-                        "{} - {}: {}",
-                        info.get_meta("timestamp").unwrap(),
-                        info.level.to_uppercase(),
-                        info.message,
-                    )
-                }),
-                // winston::format::json(),
-            ]))
-            .transports(vec![console_transport])
-            .build();
-    */
-    /*let mut logger = winston::Logger::default();
-    logger.close();*/
-    //let logger = winston::Logger::default();
-    let mut logger = winston::logger_v3::Logger::new(None);
-    logger.add_transport(console_transport);
-
-    let start = Instant::now();
-    for _ in 0..ITERATIONS {
-        // logger.info(&(MESSAGE.to_owned() + " winston_v3"));
-        logger.info(&format!("{} {}", MESSAGE, "winston_v3"));
-    }
-    let elapsed = start.elapsed();
-
-    // Capture memory usage
-    epoch::advance().unwrap();
-    let allocated = stats::allocated::read().unwrap();
-
-    BenchmarkResult {
-        name: "winston_v3".to_string(),
-        elapsed: elapsed.as_secs_f64(),
-        ops: ITERATIONS as f64 / elapsed.as_secs_f64(),
-        memory_usage: allocated as f64 / 1024.0 / 1024.0, // Convert to MB
-    }
+    run_benchmark("winston_global", || {
+        for _ in 0..ITERATIONS {
+            winston::log_info!("{} {}", MESSAGE, "winston_global");
+            //logger.info(&format!("{} {}", MESSAGE, "winston_global"));
+        }
+        winston::Logger::shutdown()
+    })
 }
 
 fn main() {
@@ -294,12 +166,9 @@ fn main() {
         //bench_env_logger,
         //bench_slog,
         bench_slog_async,
-        // bench_slog_async_block,
         //bench_tracing,
         bench_winston,
-        // bench_winston_global,
-        //bench_winston_v2,
-        bench_winston_v3,
+        bench_winston_global,
     ];
     //let benchmarks = vec![bench_slog_async_block];
     let mut results = Vec::new();
